f671a7b960eaa61ac843c9d6cd21fccd8f5d8181
Andy Riedlinger <git+andy@riedlinger.me>
2026-06-09 14:20:23 -0400

    Modified/Added binary parsing examples

--- Examples/example_parsing_stored_binary.py
+++ Examples/example_parsing_stored_binary.py
@@ -1,49 +1,86 @@
-from yostlabs.tss3 import ThreespaceSensor, StreamableCommands
-from yostlabs.communication.serial import ThreespaceSerialComClass
+from yostlabs.tss3 import StreamableCommands, ThreespaceHeaderInfo
+from yostlabs.tss3.commands import threespace_command_get
 from yostlabs.tss3.utils.parser import ThreespaceBinaryParser
-import time
+from pathlib import Path
 
-#Create a sensor by auto detecting a ThreespaceSerialComClass
-sensor = ThreespaceSensor(ThreespaceSerialComClass)
+DATA_FILE = "data0.bin" #Path to the binary file containing the gathered data, EX: "session-01/data0.bin"
+SETTINGS_CFG = "settings.cfg" #Path to settings.cfg file, EX: "session-01/settings.cfg"
+COMMAND_SOURCE = "log_slots" #Should be stream_slots or log_slots based on how the data was gathered.
 
-#First, gather some data and store it in a file for this example
-sensor.writeStreamSlots(f"{StreamableCommands.GetTimestamp.value},{StreamableCommands.GetAllPrimaryCorrectedData.value}")
-sensor.writeStreamHz(500)
+#------------------------Gather configuration for the parser------------------------
+header_info = ThreespaceHeaderInfo()
+commands = []
 
-print("Gathering data for 5 seconds...")
-sensor.startStreaming()
-start_time = time.time()
-FNAME = "logged_data.bin"
-file = open(FNAME, 'wb')
-while time.time() - start_time < 5:
-    sensor.updateStreaming()
-    packet = sensor.getOldestStreamingPacket()
-    while packet is not None:
-        file.write(packet.raw_binary) #Store the raw binary for parsing later
-        packet = sensor.getOldestStreamingPacket()
-sensor.stopStreaming()
+if SETTINGS_CFG is not None:
+    #Load settings from file
+    path = Path(SETTINGS_CFG)
+    if not path.exists():
+        print(f"Error: {SETTINGS_CFG} does not exist. Please provide a valid path to the settings.cfg file.")
+        exit(1)
+    
+    settings = {}
+    with path.open('r') as fp:
+        for line in fp:
+            if line.startswith("#") or line.strip() == "" or "=" not in line:
+                continue
+            data = line.split("=", 1)
+            if len(data) != 2:
+                continue
+            key, value = data
+            settings[key.strip()] = value.strip()
+    
+    #Find the header and log_slots
+    header_info.status_enabled = int(settings["header_status"])
+    header_info.timestamp_enabled = int(settings["header_timestamp"])
+    header_info.echo_enabled = int(settings["header_echo"])
+    header_info.checksum_enabled = int(settings["header_checksum"])
+    header_info.serial_enabled = int(settings["header_serial"])
+    header_info.length_enabled = int(settings["header_length"])
 
-file.close()
+    command_string: str = settings[COMMAND_SOURCE]
+    command_string = command_string.split(",")
+    for command in command_string:
+        #Some commands have additional info after a ":" that is not needed for parsing, so split and take the first part
+        command = command.split(":")[0].strip()
+        if command == "" or command == "255":
+            continue
+        try:
+            command_id = int(command)
+            commands.append(threespace_command_get(command_id))
+        except ValueError:
+            print(f"Warning: {command} is not a valid command ID. Skipping.")
+else:
+    #Set configuration settings manually
+    header_info.status_enabled = 0
+    header_info.timestamp_enabled = 0
+    header_info.echo_enabled = 0
+    header_info.checksum_enabled = 1
+    header_info.serial_enabled = 0
+    header_info.length_enabled = 0
 
-#Create the parser
+    commands = [threespace_command_get(StreamableCommands.GetTaredOrientation.value), 
+                threespace_command_get(StreamableCommands.GetPrimaryCorrectedAccelVec.value)]
+
+
+#--------------------Create the parser and configure it--------------------
 parser = ThreespaceBinaryParser(verbose=True)
-
-#Configure it to have the same settings used so it knows how to read the data
-parser.set_header(sensor.header_info)
+parser.set_header(header_info)
 
 #Register all command responses that could have been included. This includes more then streaming data,
-#however because this example did streaming, it is going to register the getStreamingBatch command to parse.
-#Some commands take additional KWARGS to know how to parse. Ex: getStreamingBatch requires knowing what the streaming slots were
-parser.register_command(84, stream_slots=sensor.streaming_slots)
+#but this example is purely for parsing streaming/logging data, so it must register the getStreamingBatch command.
+parser.register_command(84, stream_slots=commands)
+
+#--------------------------------Load Data--------------------------------
 
 #Once all commands are registered, simply feed the parser data and attempt to retrieve messages
-file = open(FNAME, "rb")
+with open(DATA_FILE, "rb") as fp:
+    parser.insert_data(fp.read())
+
+#--------------------------------Parse Data--------------------------------
 
 print("Parsing the gathered data from the binary file")
-parser.insert_data(file.read())
 msg = parser.parse_message()
 while msg is not None:
     print(msg)
     msg = parser.parse_message()
-
-file.close()
+print("Done.")
