1c654f3d424fac6a368392ebd01b30b91784be76
Andy Riedlinger <git+andy@riedlinger.me>
2026-08-18 17:24:46 -0400

    Self test format updated

--- packages/yostlabs/src/yostlabs/tss3/utils/tests/test_self.py
+++ packages/yostlabs/src/yostlabs/tss3/utils/tests/test_self.py
@@ -4,30 +4,22 @@
 # were missed, and may give additional information in the
 # case of failures.
 
-from yostlabs.tss3.utils.tests.base import SensorTestBase
+from yostlabs.tss3.utils.tests.base import SensorTestBase, TestResult, TestStatus
 from yostlabs.tss3.api import ThreespaceSensor, InvalidKeyError
 from yostlabs.tss3.consts import *
 import time
 
 class SelfTest(SensorTestBase):
 
+    # Keys must be in order of the bits in the self test result bitfield.
+    BIT_KEYS = ["accel", "gyro", "mag", "baro", "rtc", "gps", "bluetooth", "sd", "sms", "battery"]
+
     def __init__(self, sensor: ThreespaceSensor):
         super().__init__(sensor)
 
         self.settings_cache = {}
-        # Initialize dict. Keys must be in order of the bits in the result.
-        self.result = {
-            "raw": 0,
-            "accel": True,
-            "gyro": True,
-            "mag": True,
-            "baro": True,
-            "rtc": True,
-            "gps": True,
-            "bluetooth": True,
-            "sd": True,
-            "sms": True,
-            "battery": True
+        self.result: dict[str, TestResult] = {
+            "bitfield": TestResult("self", "bitfield"),
         }
 
     def __cache_settings(self):
@@ -60,14 +52,14 @@ class SelfTest(SensorTestBase):
     def start(self):
         self.__cache_settings()
 
-        result = self.sensor.selfTest().data
-        self.result["raw"] = result
+        result = self.result["bitfield"]
+        raw = self.sensor.selfTest().data
+        result.measurements["raw"] = raw
 
-        keys = list(self.result.keys())
-        keys.remove("raw")
-        for i, key in enumerate(keys):
-            self.result[key] = not bool(result & (1 << i))
-        self.overall_success = (result == 0)
+        for i, key in enumerate(self.BIT_KEYS):
+            result.measurements[key] = not bool(raw & (1 << i))
+
+        result.set_status(TestStatus.PASS if raw == 0 else TestStatus.FAIL)
         self.__restore_settings()
 
     def cancel(self):
@@ -76,13 +68,14 @@ class SelfTest(SensorTestBase):
 def run_test(sensor: ThreespaceSensor):
     test = SelfTest(sensor)
     test.start()
-    return test.overall_success, test.result
+    return test.overall_success, test.result_flat
 
 def auto_run_test():
     sensor = ThreespaceSensor()
     overall_success, results = run_test(sensor)
     sensor.cleanup()
-    print(results)
+    for test in results:
+        print(test)
     print("Overall success:", overall_success)
     return overall_success, results
 
